f3ef936d4527d9a21e80d884d41a05969ce2e1b3
programmer-man <bryan@kerigan.com>
2018-02-03 14:50:17 -0500

    add tagmanager

--- wp-content/themes/fbcpsj/template-parts/partials/head.php
+++ wp-content/themes/fbcpsj/template-parts/partials/head.php
@@ -10,8 +10,19 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PS6ZCBN');</script>
+    <!-- End Google Tag Manager -->
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?> >
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PS6ZCBN"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'fbcpsj' ); ?></a>
