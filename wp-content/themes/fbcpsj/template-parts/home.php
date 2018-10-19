8c00c40d7c19bcbcbd9f31d03f8b6664de01c6c6
Bryan Baird <bryan@kerigan.com>
2018-10-19 11:17:44 -0400

    comments

--- wp-content/themes/fbcpsj/template-parts/home.php
+++ wp-content/themes/fbcpsj/template-parts/home.php
@@ -73,6 +73,6 @@ $subhead = ($post->page_information_subhead != '' ? $post->page_information_subh
 
     </div>
     <div id="news" class="section-wrapper" >
-        <?php include(locate_template('template-parts/partials/news-feed.php')); ?>
+        <?php //include(locate_template('template-parts/partials/news-feed.php')); ?>
     </div>
 </div>
