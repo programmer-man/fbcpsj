c210b565fd8adb2d9fcba693a6cecfeb053ca683
Bryan Baird <bryan@kerigan.com>
2020-03-19 22:50:11 -0400

    local updates

--- wp-content/plugins/simple-custom-post-order/assets/scporder.js
+++ wp-content/plugins/simple-custom-post-order/assets/scporder.js
@@ -31,23 +31,34 @@
     /****
      * Fix for table breaking
      */
+    jQuery(window).load(function () {
+        jQuery('#the-list').width(jQuery('#the-list').width());
 
-    $('#the-list').on('mousedown', function () {
-        jQuery('#the-list').parent().find('thead').find('th').each(function () {
-            $(this).width($(this).width())
+        // make the array for the sizes
+        var td_array = new Array();
+        var i = 0;
+        jQuery('#the-list tr:first-child').find('td').each(function () {
+            td_array[i] = $(this).outerWidth();
+            i += 1;
         });
-        jQuery('#the-list').find('td').each(function () {
-            $(this).width($(this).width())
-        });
-    });
 
-    $('#the-list').on('mouseup', function () {
-        jQuery('#the-list').parent().find('thead').find('th').each(function () {
-            $(this).removeAttr('style');
+        jQuery('#the-list').find('tr').each(function () {
+            var j = 0;
+            $(this).find('td').each(function () {
+                $(this).width(td_array[j]);
+                j += 1;
+            });
         });
-        jQuery('#the-list').find('td').each(function () {
-            $(this).removeAttr('style');
-        });
+
+        var y = 0;
+        // check if there are no items in the table
+        if(jQuery('#the-list > tr.no-items').length == 0){
+            jQuery('#the-list').parent().find('thead').find('th').each(function () {
+                $(this).width(td_array[y]);
+                y += 1;
+            });
+        }
+
     });
 
     /*****
